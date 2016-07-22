8882d550d53aaeaa4b1bb4cd31a269d209679cfa
mlarcode <sightsnsenses@gmail.com>
2016-07-21 21:05:21 -0400

    second commit

---
+++ OnPar Problem 2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnPar_Problem_2
+{
+    class randomNum
+    {
+        static void Main(string[] args)
+        {
+            // Enter 1st number
+            int a;
+            Console.WriteLine("Enter a number:");
+            Console.ReadKey();
+            
+            //Enter operation
+            Console.WriteLine("Enter math operation:");
+            char[] operators = { '+', '-', '*', '/' };
+            char op = operators[r.Next(operators.Length)];
+            Console.ReadKey();
+
+            //Enter 2nd number
+            int b;
+            Console.WriteLine("Enter a number:");
+            Console.ReadKey();
+            {
+                op = answer
+                switch (op)
+                {
+                    case '+':
+                        answer = a + b;
+                        break;
+
+                    case '-':
+                        answer = a - b;
+                        break;
+
+                    case '*':
+                        answer = a * b;
+                        break;
+
+                    case '/':
+                        answer = a / b;
+                        break;
+
+                        Console.WriteLine(op answer);
+                        Console.ReadKey();
+                        Console.Write("Do you want to enter another number?(YES/No):" + "\n");
+                }
+                char y = (char)Console.Read();
+
+                if (Char.IsLetter(y))
+                {
+                    if (Char.IsUpper(y))
+                    {
+                        Console.WriteLine("Continue");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Goodbye:");
+                    }
+
+                }
+    }
+
